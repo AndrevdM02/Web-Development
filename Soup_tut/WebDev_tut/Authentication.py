237be0c671dddee8d77ae78295787e8ddb16613a
Andre Dirk Van Der Merwe <24923273@sun.ac.za>
2025-01-19 11:48:44 +0200

    Computer Science 343

--- Soup_tut/WebDev_tut/Authentication.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-from requests.auth import HTTPBasicAuth
-
-basic = HTTPBasicAuth('user', 'pass')
-page = requests.get('https://httpbin.org/basic-auth/user/pass', auth=basic)
-print(page.text)
